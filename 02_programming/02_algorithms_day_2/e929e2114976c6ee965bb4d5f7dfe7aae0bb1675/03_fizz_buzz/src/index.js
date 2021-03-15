448d608b6f797ce1da4b8531b9aa912486aedbda
DavidAmegavi <amegavi.dav@gmail.com>
2021-03-15 09:54:39 +0100

    exo 3 modif

--- 02_programming/02_algorithms_day_2/e929e2114976c6ee965bb4d5f7dfe7aae0bb1675/03_fizz_buzz/src/index.js
+++ 02_programming/02_algorithms_day_2/e929e2114976c6ee965bb4d5f7dfe7aae0bb1675/03_fizz_buzz/src/index.js
@@ -1,6 +1,23 @@
-function fizzBuzz() {
+function fizzBuzz(number1, number2, func) {
   // Code the function here.
+  const list=[];
+
+  if (number1 < number2) { 
+    let n = number1
+
+    while (n <= number2) {
+      n++;
+      list.push(n);
+
+    }
+  }
+  if (n % 3 === 0 ){
+    console.log("fizz")
+  }
+  return list;
+  
 }
 
 // Leave the line below untouched for tests to work properly.
 module.exports = fizzBuzz;
+console.log(fizzBuzz(0, 14)); 
