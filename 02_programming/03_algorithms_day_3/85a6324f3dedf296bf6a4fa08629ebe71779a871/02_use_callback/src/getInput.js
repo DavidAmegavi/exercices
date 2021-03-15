2f2bdc7d365713fcf8a42c1416225d922a7cc685
DavidAmegavi <amegavi.dav@gmail.com>
2021-03-15 14:10:10 +0100

    Modif exo

---
+++ 02_programming/03_algorithms_day_3/85a6324f3dedf296bf6a4fa08629ebe71779a871/02_use_callback/src/getInput.js
@@ -0,0 +1,10 @@
+// A function that takes two parameters, the second one being a callback function
+
+const getInput = (user, callback) => {
+  console.log(user, callback("A"));
+   
+
+}; 
+
+// Leave the line below for tests to work
+module.exports = getInput;
