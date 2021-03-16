3deffe3f848d967db813a3740a8525fa93665e72
DavidAmegavi <amegavi.dav@gmail.com>
2021-03-16 17:54:43 +0100

    Exo 3 almost done

--- 02_programming/04_your_first_program/8cdc56df78d3f37db11352eb2b411fe3d71d6fc6/03_number_game/src/numberGame.js
+++ 02_programming/04_your_first_program/8cdc56df78d3f37db11352eb2b411fe3d71d6fc6/03_number_game/src/numberGame.js
@@ -1,5 +1,25 @@
 function numberGame(reader, min = 1, max = 100) {
-  // code here
-}
+  let nbComputer = Math.round(Math.random() * (max - min) + min);
+  console.log(nbComputer);
 
+  function game(plop) {
+    const nb = parseInt(plop);
+
+    if (!Number.isInteger(nb)) {
+      reader.question("This was not a number ", game);
+      console.log("Enter a number ");
+    } else if (nb < min || nb > max) {
+      reader.question("The number is between 1 and 100", game);
+      console.log("Enter a number ");
+    } else if (nb < nbComputer) {
+      reader.question("Too low ", game);
+      console.log("Enter a number ");
+    } else if (nb > nbComputer) {
+      reader.question("Too high", game);
+      console.log("Enter a number ");
+    } else if (nb === nbComputer) console.log("You won!");
+    reader.close();
+  }
+  reader.question("Enter a number ", game);
+}
 module.exports = numberGame;
