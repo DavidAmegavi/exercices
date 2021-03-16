089d05387ed234d9b3ba163c83beed487c3c7a91
DavidAmegavi <amegavi.dav@gmail.com>
2021-03-16 10:29:48 +0100

    New training

--- 02_programming/01_algorithms_day_1/c2fee3d27668042d64eb9d05e174fd5e9db87123/04_play_with_for_loop/src/04_filtering.js
+++ 02_programming/01_algorithms_day_1/c2fee3d27668042d64eb9d05e174fd5e9db87123/04_play_with_for_loop/src/04_filtering.js
@@ -3,5 +3,12 @@
 // -  Create an array called `literalDigits` from `zero` to `nine` where each array entry is a spelled-out number;
 // -  Using `length`, write on `stdout` each odd values of the table.
 let literalDigits = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]; 
-let literalDigitsModulo = literalDigits[literalDigits] % 2; 
-console.log(literalDigits.length[0]); 
+let literalDigitsLength = literalDigits.length; 
+
+for( let i = [0]; i < literalDigitsLength; i++ ){ 
+  if (literalDigits[i] % 2 === 1 ){
+    console.log(literalDigits[i]);
+  }   
+}
+
+  
--- 02_programming/03_algorithms_day_3/85a6324f3dedf296bf6a4fa08629ebe71779a871/04_square_digits/src/squareDigits.js
+++ 02_programming/03_algorithms_day_3/85a6324f3dedf296bf6a4fa08629ebe71779a871/04_square_digits/src/squareDigits.js
@@ -1,6 +1,17 @@
-function squareDigits() {
-  // Code here
+function squareDigits(integer) {
+   
+  
+  if ( Number.isInteger(integer = true )){
+    let result = integer * integer ;
+    return result;  
+  }
+  else if (Number.isInteger(integer = false )) {
+    let resultError = new error ("This is not an number !");
+    throw resultError;
+ 
+  }
 }
 
 // Leave the line below for tests to work
 module.exports = squareDigits;
+console.log(squareDigits(6.9)); 
