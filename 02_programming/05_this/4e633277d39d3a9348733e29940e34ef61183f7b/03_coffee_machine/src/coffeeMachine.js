f199de471ec94be5c075079ec20211ab40377b79
DavidAmegavi <amegavi.dav@gmail.com>
2021-03-17 14:33:25 +0100

    new update

--- 02_programming/05_this/4e633277d39d3a9348733e29940e34ef61183f7b/03_coffee_machine/src/coffeeMachine.js
+++ 02_programming/05_this/4e633277d39d3a9348733e29940e34ef61183f7b/03_coffee_machine/src/coffeeMachine.js
@@ -1,5 +1,27 @@
 const machine = {
-  // Complete here
+  litersOfCoffee: 0, 
+  fillWithLitersOfCoffee: function(quantity){
+    this.litersOfCoffee += quantity; 
+    return this; 
+  },
+  expresso: function () {
+    if(this.litersOfCoffee >= 0.08){
+      this.litersOfCoffee -= 0.08;
+      return true; 
+    }
+    else {
+      return false; 
+    }
+  },
+  longCoffee: function() {
+    if(this.litersOfCoffee >= 0.15){
+      this.litersOfCoffee -= 0.15;
+      return true; 
+    }
+    else{
+      return false; 
+    }
+  }
 };
 
 module.exports = machine;
