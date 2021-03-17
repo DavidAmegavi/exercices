0d60d6796e459dea1d1b00e98eeb8bfaa762ab88
DavidAmegavi <amegavi.dav@gmail.com>
2021-03-17 09:58:12 +0100

    New update

--- 02_programming/04_your_first_program/8cdc56df78d3f37db11352eb2b411fe3d71d6fc6/04_number_game_with_stats/src/numberGameWithStats.js
+++ 02_programming/04_your_first_program/8cdc56df78d3f37db11352eb2b411fe3d71d6fc6/04_number_game_with_stats/src/numberGameWithStats.js
@@ -1,5 +1,38 @@
 function numberGameWithStats(reader, min = 1, max = 100) {
+  let nbComputer = Math.round(Math.random() * (max - min) + min);
+  console.log(nbComputer);
 
+  console.log("Welcome!\nYou have to find the right number, between 1 and 100!\nGood luck!!");
+  reader.question("Enter a number", game);
+  function numberOfTries (count){
+    if(enter )
+  }
+
+  function game(enter) {
+    const nb = parseInt(enter);
+    if (Number.isInteger(nb)){
+      if(nb < min || nb > max ){
+        console.log("The number is between 1 and 100");
+        reader.question("Enter a number", game);
+      }
+      else if (nb < nbComputer){
+        console.log("Too low");
+        reader.question("Enter a number", game);
+      }
+      else if (nb > nbComputer){
+        console.log("Too high");
+        reader.question("Enter a number", game);
+      }
+      else if (nb === nbComputer || numberOfTries === 7){
+        console.log("You won!");
+        reader.close();
+      }
+    }
+    else {
+      console.log("This was not a number ");
+      reader.question("Enter a number", game);
+    }
+  }
 }
 
 module.exports = numberGameWithStats;
