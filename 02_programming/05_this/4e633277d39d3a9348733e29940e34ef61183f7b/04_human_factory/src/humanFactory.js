2f46f374c6cececd3573a1d55be05f435f545cec
DavidAmegavi <amegavi.dav@gmail.com>
2021-03-18 00:19:02 +0100

    UpdateHumanFactory

--- 02_programming/05_this/4e633277d39d3a9348733e29940e34ef61183f7b/04_human_factory/src/humanFactory.js
+++ 02_programming/05_this/4e633277d39d3a9348733e29940e34ef61183f7b/04_human_factory/src/humanFactory.js
@@ -1,9 +1,27 @@
 const humanFactory = (humanData = {}) => {
-  // code here
+  const human = {
+    firstname: "John", 
+    lastname: "Doe", 
+    genre: "male", 
+    job: "unemployed", 
+    fullname: function () {
+      return (`${this.firstname} ${this.lastname}`); 
+    },
+    introduction: function () {
+      return (`Hello! My name is ${this.fullname()}.`);
+    },
+  };
+  if(humanData.genre)
+    return {...human,...humanData};
 };
+const humans = humanFactory({ lastName: "Toto" });
 
+const createHumans = (array) => {return array.map(humanFactory);};
+
+console.log(humans);
 const createHumans = (humans) => {
-  // code here
+
+  
 };
 
 module.exports = {
