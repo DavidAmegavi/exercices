1c7bcc5168f3ce65af64f9271b7b79a14fc6bf0d
DavidAmegavi <amegavi.dav@gmail.com>
2021-03-22 15:19:18 +0100

    Exo 1 update

---
+++ 02_programming/08_Reboot_day_1/2bd2a813fc43ac5e741a903d557ea1cf5bf5888a/01_reboot_calculator/src/calculator.js
@@ -0,0 +1,50 @@
+function calculator(reader) {
+  console.log("Welcome to your calculator!");
+  const askFirstNumber = () => {
+    reader.question("Enter your first number: \n", (value1) => {
+      if (checkNumber(value1) !== value1) {
+        console.log("Not a number");
+        askFirstNumber();
+      } else {
+        reader.question("Enter the operation [+ * / -]: \n", (operation) => {
+          const askSecondNumber = () => {
+            reader.question("Enter your second number: \n", (value2) => {
+              if (checkNumber(value2) !== value2) {
+                console.log("Not a number");
+                askSecondNumber();
+              } else {
+                function calcul() {
+                  if (operation === "+") {
+                    let result = parseInt(value1) + parseInt(value2);
+                    console.log(result);
+                  } else if (operation === "*") {
+                    let result = parseInt(value1) * parseInt(value2);
+                    console.log(result);
+                  } else if (operation === "/") {
+                    let result = parseInt(value1) / parseInt(value2);
+                    console.log(result);
+                  } else if (operation === "-") {
+                    let result = parseInt(value1) - parseInt(value2);
+                    console.log(result);
+                  }
+                  reader.close();
+                }
+                calcul();
+              }
+            });
+          };
+          askSecondNumber();
+        });
+      }
+    });
+  };
+  askFirstNumber();
+}
+function checkNumber(value) {
+  if (isNaN(value)) {
+    return null;
+  } else {
+    return value;
+  }
+}
+export default calculator;
