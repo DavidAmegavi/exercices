e4272f551f35add6c8efb894859b7982d47ee7ad
DavidAmegavi <amegavi.dav@gmail.com>
2021-03-24 16:17:31 +0100

    update exo 3

--- 02_programming/10_typescript_101/d7806418db17545e4bdeebe9186a600c561b20c7/03_typed_class/src/index.ts
+++ 02_programming/10_typescript_101/d7806418db17545e4bdeebe9186a600c561b20c7/03_typed_class/src/index.ts
@@ -1,6 +1,30 @@
+
 class Bird {
-  // Write your code here.
+ age: number;
+
+
+constructor(ageBird:number){
+  this.age = ageBird;
 }
+
+ sing(): void {
+  console.log("Cui cui"); 
+}
+
+fly(time: number): void {
+  if(this.age <= 1){
+    console.log("The bird is too young to fly");
+  } else {
+    if(this.age > 1 && this.age <= 3){
+      console.log(`The bird flew ${time * 1} in ${time} seconds`);
+    }else if (this.age > 3){
+      console.log(`The bird flew ${time * 2} in ${time} seconds`);
+    }
+  }
+}
+}
+const myBird = new Bird(2);
+console.log(myBird); 
 
 // Leave the line below for tests to work properly.
 export { Bird };
