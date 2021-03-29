8693d234f7f3cedac0ff6594360a036b1c0c37f5
DavidAmegavi <amegavi.dav@gmail.com>
2021-03-29 15:35:11 +0200

    update exo 2

--- 03_backend_web_development_and_api/01_promises/a15b4e3fc4cb48d99dab2e50e0efa06f38382289/02_chuck/src/chuck.ts
+++ 03_backend_web_development_and_api/01_promises/a15b4e3fc4cb48d99dab2e50e0efa06f38382289/02_chuck/src/chuck.ts
@@ -1,11 +1,17 @@
 import { getChuckCategories, getChuckJoke } from "../utils";
 
-function getCategories() {
+function getCategories(): Promise<string[]> {
   // Your code goes here
+  return new Promise((resolve) => {
+    const categorieInput = getChuckCategories();
+    resolve(categorieInput);
+  });
 }
-
-function getJoke() {
+function getJoke(category: string): Promise<string> {
   // Your code goes here
+  return new Promise((resolve) => {
+    resolve(getChuckJoke(category).then((joke) => joke.value));
+  });
 }
 
 // Leave the line below for tests to work properly
