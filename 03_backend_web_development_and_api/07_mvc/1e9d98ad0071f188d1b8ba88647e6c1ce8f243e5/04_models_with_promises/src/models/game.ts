7519f67080ae36710c11f579935f7bd6a5ad5758
DavidAmegavi <amegavi.dav@gmail.com>
2021-04-14 08:59:57 +0200

    update mvc

---
+++ 03_backend_web_development_and_api/07_mvc/1e9d98ad0071f188d1b8ba88647e6c1ce8f243e5/04_models_with_promises/src/models/game.ts
@@ -0,0 +1,74 @@
+export type Game = {
+  name: string;
+  slug: string;
+  [key: string]: any
+};
+
+export type Platform = {
+  name: string;
+  slug: string;
+  [key: string]: any
+};
+
+const result :Platform[] = [];
+
+export class GameModel {
+  collection: Game[];
+
+  constructor(collection: Game[]) {
+    this.collection = collection
+  }
+
+  getAll(): Promise <Game[]> {
+    return new Promise((resolve) => {
+      resolve(
+        this.collection.map(game => {
+          return {
+            name: game.name, 
+            slug: game.slug, 
+            cover: game.cover_url,
+          };
+        }),
+      );
+    }); 
+  }
+
+  findBySlug(slug: string): Promise<Game | null> {
+  const getGameBySlug = this.collection.find(game => {
+    return game.slug === slug
+  })
+    if(getGameBySlug === undefined){
+      return null
+    }
+  resolve(getGameBySlug)
+}
+
+  findByPlatform(platform_slug: string): Game[] {
+     return this.collection.filter(game =>{
+      if(game.platform.slug === platform_slug){
+        return {
+          name: game.name,
+          slug: game.slug, 
+        }
+      }
+    })   
+  }
+  
+
+  getPlatforms(): Platform[] {
+    this.collection.forEach((game) => {
+      const firstStep = result.find((element) => {
+        return element.slug === game.platform.slug
+      })
+      if(!firstStep){
+        result.push({
+          name: game.platform.name, 
+          slug: game.platform.slug
+        })
+      }
+    })
+    return (result);  
+  }
+  
+
+}
